894046108b54dceeefdc1446b186f3f259453a91
EmiliaIF <89966124+EmiliaIF@users.noreply.github.com>
2022-05-25 10:37:02 +0200

    fixed some fonts and made the code a little prettier

--- receptsajten2/backend/src/models/recept.ts
+++ receptsajten2/backend/src/models/recept.ts
@@ -1,105 +1,86 @@
-
 import { Schema, model } from "mongoose";
-const { stringify } = require('nodemon/lib/utils');
+const { stringify } = require("nodemon/lib/utils");
 
+export interface Recept {
+  title: String;
 
-export interface Recept{
-  
-    title: 
-    String,
+  description: String;
 
-    description:
-    String,
- 
-    imageUrl: 
-    String,
+  imageUrl: String;
 
-    timeInMins: 
-    Number,
-        
+  timeInMins: Number;
 
-    category: 
-    string
-       
-   
-    ingredients: 
-    [{name: string, amount: number, unit: string}]
-       
+  category: string;
 
-    instructions:
-    [{instruction: string, order: number,}]
-        
+  ingredients: [{ name: string; amount: number; unit: string }];
 
-    ratings: 
-     [number]
-   
-    comments?:
-     [{comment: string, name: string, createdAt: Date}]
-  
+  instructions: [{ instruction: string; order: number }];
 
-};
+  ratings: [number];
 
+  comments?: [{ comment: string; name: string; createdAt: Date }];
+}
 
 const ReceptSchema = new Schema<Recept>({
-   
-    title: {
-        type: String,
-        required: true
-    },
-    description: {
-        type: String,
-        required: true
-    },
-    imageUrl: {
-        type: String,
-        required: true
-    },
-    timeInMins: {
-        type: Number,
-        required: true
-    },
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  timeInMins: {
+    type: Number,
+    required: true,
+  },
 
-    category: {
-        type: String,
-        required: true
-    },
-    ingredients: {
-        type: [{
-         name: String,
-         amount: Number,
-         unit: String,
+  category: {
+    type: String,
+    required: true,
+  },
+  ingredients: {
+    type: [
+      {
+        name: String,
+        amount: Number,
+        unit: String,
+      },
+    ],
+    required: true,
+  },
 
-    }],
-    required: true},
-  
+  instructions: {
+    type: [
+      {
+        instruction: String,
+        order: Number,
+      },
+    ],
 
-    instructions: {
-    type: [{
-         instruction: String,
-         order: Number,
-        }],
+    required: true,
+  },
 
-        required: true},
+  comments: {
+    type: [
+      {
+        comment: String,
+        name: String,
+        createdAt: Date,
+      },
+    ],
+    required: false,
+  },
 
-
-    comments: {
-        type: [{
-         comment: String,
-         name: String,
-         createdAt: Date,
-
-        }],
-        required: false
-    },
-
-    ratings: {
-        type: [Number],
-        required: false
-    },
-   
-   
-
+  ratings: {
+    type: [Number],
+    required: false,
+  },
 });
 
-const Recept = model('Recept', ReceptSchema);
+const Recept = model("Recept", ReceptSchema);
 export default Recept;
